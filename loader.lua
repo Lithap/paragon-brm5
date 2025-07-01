2db4cc38246a9b91e36d0846af95538b34f2d48e
Josh <118329788+Lithap@users.noreply.github.com>
2025-06-30 21:51:18 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -15,7 +15,7 @@ ScriptContext.Error:Connect(function(m) if swallow(m) then return true end end)
 ---------------------------------------------------------------------
 -- 1. Fetch Parvus openworld.lua and keep only ESP core ---------------
 ---------------------------------------------------------------------
-local src = game:HttpGet("https://raw.githubusercontent.com/Lithap/paragon-brm5/main/openworld.lua")
+local src = game:HttpGet("https://raw.githubusercontent.com/Lithap/paragon-brm5/main/esp.lua")
 assert(src and #src>2000, "Failed to download openworld.lua")
 -- extract everything before the GUI block
 local core = src:match("^(.-)%-%-%s*GUI")
